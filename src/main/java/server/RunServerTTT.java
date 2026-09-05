e2295b3274747448fab15711cc6133b935111152
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2026-09-05 11:30:54 +0200

    fix: merge conflict, server in remote

--- src/main/java/server/RunServerTTT.java
+++ src/main/java/server/RunServerTTT.java
@@ -3,30 +3,34 @@ package main.java.server;
 import main.java.GameManager;
 import main.java.GameManagerImpl;
 
+import java.net.InetAddress;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class RunServerTTT {
     public static void main(String[] args) {
-
         try {
-            GameManager manager = new GameManagerImpl();
+            // Se passi l'IP da terminale usa quello, altrimenti lo rileva in automatico
+            String hostIp = (args.length > 0) ? args[0] : InetAddress.getLocalHost().getHostAddress();
 
+            System.setProperty("java.rmi.server.hostname", hostIp);
+            System.out.println("[Server] RMI Hostname configurato su: " + hostIp);
+
+            GameManager manager = new GameManagerImpl();
             var gameStub = (GameManager) UnicastRemoteObject.exportObject(manager, 0);
 
             //try creato in modo che se la porta è già occupata, recupera il registro esistente
             Registry registry;
             try {
+                //prova a creare un nuovo registro RMI
                 registry = LocateRegistry.createRegistry(1099);
             } catch (java.rmi.server.ExportException e) {
                 registry = LocateRegistry.getRegistry(1099);
             }
 
-
             registry.rebind("GameManager", gameStub );
-
-            System.out.println("[Server] Server Started");
+            System.out.println("[Server] Server Started e in ascolto");
 
         } catch (Exception e) {
             System.err.println("exception:" + e.toString());
